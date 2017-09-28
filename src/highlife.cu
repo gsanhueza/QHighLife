264207193889b6ed6c06a793df8e648a895ef6f9
gsanhueza <gabriel_8032@hotmail.com>
2017-09-27 21:42:03 -0300

    Ajustando ideas para CUDA, aun no puede hacer modificaciones

--- src/highlife.cu
+++ src/highlife.cu
@@ -28,38 +28,44 @@ __global__ void computeHighLife(Grid *grid, Grid *result)
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     int j = blockDim.y * blockIdx.y + threadIdx.y;
 
-    if (i < getWidth(grid) and j < getHeight(grid) and i >= 0 and j >= 0)
-    {
-        setAt(result, i, j, !getAt(grid, i, j));
-    }
+    setAt(result, 1, 0, false);
+//     if (i < getWidth(grid) and j < getHeight(grid) and i >= 0 and j >= 0)
+//     {
+//         setAt(result, i, j, !getAt(grid, i, j));
+//     }
 }
 
 // Cuda main
 extern "C"
 int cuda_main(Grid *grid)
 {
-    Grid *mygrid = new Grid(grid->getWidth(), grid->getHeight());
-    Grid *result = new Grid(grid->getWidth(), grid->getHeight());
-
-    *mygrid = *grid;
-    *result = *grid;
+    Grid *mygrid;
+    Grid *result;
 
     cudaMallocManaged(&mygrid, sizeof(mygrid));
     cudaMallocManaged(&result, sizeof(result));
+
+    mygrid = new Grid(grid->getWidth(), grid->getHeight());
+    result = new Grid(grid->getWidth(), grid->getHeight());
+
+    *mygrid = *grid;
+    *result = *grid;
 
     int blocksize = 32;
     dim3 threads(blocksize, blocksize);
     dim3 cudagrid(mygrid->getWidth() / threads.x, mygrid->getHeight() / threads.y);
 
+    // FIXME Cuda puede hacer modificaciones (TODO), pero hay que ponerle ojo a los margenes, o hace segfault
     std::cout << "CUDA can receive a Grid object" << std::endl;
     std::cout << &mygrid << std::endl;
-    std::cout << "mygrid.getAt(0, 0) was = " << mygrid->getAt(0, 0) << std::endl; // SEGFAULT
-    std::cout << "result.getAt(0, 0) was = " << result->getAt(0, 0) << std::endl;
+    std::cout << "mygrid.getAt(1, 0) was = " << std::boolalpha << mygrid->getAt(1, 0) << std::endl;
+    std::cout << "result.getAt(1, 0) was = " << std::boolalpha << result->getAt(1, 0) << std::endl;
     computeHighLife<<< cudagrid, threads >>>(mygrid, result);
     std::cout << "CUDA can send a Grid object" << std::endl;
-    std::cout << "mygrid.getAt(0, 0) is = " << std::boolalpha << mygrid->getAt(0, 0) << std::endl;
-    std::cout << "result.getAt(0, 0) is = " << std::boolalpha << result->getAt(0, 0) << std::endl;
+    std::cout << "mygrid.getAt(1, 0) is = " << std::boolalpha << mygrid->getAt(1, 0) << std::endl;
+    std::cout << "result.getAt(1, 0) is = " << std::boolalpha << result->getAt(1, 0) << std::endl;
 
-    *mygrid = *result;
+    // Final result
+    *grid = *result;
     return 0;
 }
