134944a3fea0e9510737933376940fb5e9f9805a
gsanhueza <gabriel_8032@hotmail.com>
2017-09-27 22:59:02 -0300

    Se cambió la lógica de pasado de grid al canvas, si se carga un modelo, se cambian los colores para indicar listo-para-correr

--- src/highlife.cu
+++ src/highlife.cu
@@ -2,33 +2,13 @@
 #include <cuda_runtime.h>
 #include "grid.h"
 
-__host__ __device__ bool getAt(Grid *grid, int i, int j)
-{
-    return grid->getAt(i, j);
-}
-
-__host__ __device__ void setAt(Grid *grid, int i, int j, bool value)
-{
-    grid->setAt(i, j, value);
-}
-
-__host__ __device__ int getWidth(Grid *grid)
-{
-    return grid->getWidth();
-}
-
-__host__ __device__ int getHeight(Grid *grid)
-{
-    return grid->getHeight();
-}
-
 // Kernel
-__global__ void computeHighLife(Grid *grid, Grid *result)
+__global__ void computeHighLife(bool **grid, bool **result, int width, int height)
 {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     int j = blockDim.y * blockIdx.y + threadIdx.y;
 
-    setAt(result, 1, 0, false);
+    result[1][0] = false;
 //     if (i < getWidth(grid) and j < getHeight(grid) and i >= 0 and j >= 0)
 //     {
 //         setAt(result, i, j, !getAt(grid, i, j));
@@ -39,33 +19,30 @@ __global__ void computeHighLife(Grid *grid, Grid *result)
 extern "C"
 int cuda_main(Grid *grid)
 {
-    Grid *mygrid;
-    Grid *result;
+    bool **innerGrid;
+    bool **innerResult;
 
-    cudaMallocManaged(&mygrid, sizeof(mygrid));
-    cudaMallocManaged(&result, sizeof(result));
+    cudaMallocManaged(&innerGrid, grid->getWidth() * grid->getHeight() * sizeof(bool));
+    cudaMallocManaged(&innerGrid, grid->getWidth() * grid->getHeight() * sizeof(bool));
 
-    mygrid = new Grid(grid->getWidth(), grid->getHeight());
-    result = new Grid(grid->getWidth(), grid->getHeight());
-
-    *mygrid = *grid;
-    *result = *grid;
+    innerGrid = grid->getInnerGrid();
+    innerResult = grid->getInnerGrid();
 
     int blocksize = 32;
     dim3 threads(blocksize, blocksize);
-    dim3 cudagrid(mygrid->getWidth() / threads.x, mygrid->getHeight() / threads.y);
+    dim3 cudagrid(grid->getWidth() / threads.x, grid->getHeight() / threads.y);
 
     // FIXME Cuda puede hacer modificaciones (TODO), pero hay que ponerle ojo a los margenes, o hace segfault
     std::cout << "CUDA can receive a Grid object" << std::endl;
-    std::cout << &mygrid << std::endl;
-    std::cout << "mygrid.getAt(1, 0) was = " << std::boolalpha << mygrid->getAt(1, 0) << std::endl;
-    std::cout << "result.getAt(1, 0) was = " << std::boolalpha << result->getAt(1, 0) << std::endl;
-    computeHighLife<<< cudagrid, threads >>>(mygrid, result);
+    std::cout << "mygrid.getAt(1, 0) was = " << std::boolalpha << innerGrid[1][0] << std::endl;
+    std::cout << "result.getAt(1, 0) was = " << std::boolalpha << innerResult[1][0] << std::endl;
+    computeHighLife<<< 1, 1 >>>(innerGrid, innerResult, grid->getWidth(),  grid->getHeight());
+//     computeHighLife<<< cudagrid, threads >>>(innerGrid, innerResult, grid->getWidth(),  grid->getHeight());
     std::cout << "CUDA can send a Grid object" << std::endl;
-    std::cout << "mygrid.getAt(1, 0) is = " << std::boolalpha << mygrid->getAt(1, 0) << std::endl;
-    std::cout << "result.getAt(1, 0) is = " << std::boolalpha << result->getAt(1, 0) << std::endl;
+    std::cout << "mygrid.getAt(1, 0) is = " << std::boolalpha << innerGrid[1][0] << std::endl;
+    std::cout << "result.getAt(1, 0) is = " << std::boolalpha << innerResult[1][0] << std::endl;
 
     // Final result
-    *grid = *result;
+    grid->setInnerGrid(innerResult);
     return 0;
 }
