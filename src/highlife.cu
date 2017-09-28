6bf855f042277d8d4abdf7f089595bf7afc47f36
gsanhueza <gabriel_8032@hotmail.com>
2017-09-27 21:15:28 -0300

    WIP: (Aun no compilable) se requiere probablemente pasar el bool ** directamente para no llamar a las funciones host getAt y setAt

--- src/highlife.cu
+++ src/highlife.cu
@@ -5,8 +5,13 @@
 // Kernel
 __global__ void computeHighLife(const Grid *grid, Grid *result)
 {
-//     int i = blockDim.x * blockIdx.x + threadIdx.x;
-//     int j = blockDim.y * blockIdx.y + threadIdx.y;
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    int j = blockDim.y * blockIdx.y + threadIdx.y;
+
+    if (i < grid->getWidth() and j < grid->getHeight())
+    {
+        result->setAt(i, j, !(grid->getAt(i, j)));
+    }
 
 }
 
@@ -20,11 +25,12 @@ int cuda_main(Grid *grid)
     cudaMallocManaged(&grid, sizeof(Grid));
     cudaMallocManaged(&result, sizeof(result));
 
-    const int THREADS_PER_BLOCK = 256;
-    const int NUMBER_OF_BLOCKS = 1;
+    const int blocksize = 32;
+    dim3 threads(blocksize, blocksize);
+    dim3 cudagrid(grid->getWidth() / threads.x, grid->getHeight() / threads.y);
 
     std::cout << "CUDA can receive a Grid object" << std::endl;
-    computeHighLife<<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>>(grid, result);
+    computeHighLife<<< cudagrid, threads >>>(grid, result);
     std::cout << "CUDA can send a Grid object" << std::endl;
     *grid = *result;
     return 0;
