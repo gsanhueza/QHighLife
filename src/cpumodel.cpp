360a09dbc79b1ea916f810bdacd68869dbfbd742
gsanhueza <gabriel_8032@hotmail.com>
2017-09-30 16:13:31 -0300

    Legibilidad de código de posiciones para CPUModel

--- src/cpumodel.cpp
+++ src/cpumodel.cpp
@@ -59,22 +59,30 @@ int CPUModel::runStressTest(int timeInSeconds)
 
 int CPUModel::surroundingAliveCells(int i, int j)
 {
-    int NW, N, NE;
-    int W, E;
-    int SW, S, SE;
-
     int h = m_grid->getHeight();
     int w = m_grid->getWidth();
 
-    N = m_grid->getAt(i, (j + h - 1) % h);
-    E = m_grid->getAt((i + 1) % w, j);
-    S = m_grid->getAt(i, (j + 1) % h);
-    W = m_grid->getAt((i + w - 1) % w, j);
+    // Positions
+    int Nx = i;
+    int Ex = (i + 1) % w;
+    int Sx = i;
+    int Wx = (i + w - 1) % w;
 
-    NW = m_grid->getAt((i + w - 1) % w, (j + h - 1) % h);
-    NE = m_grid->getAt((i + 1) % w, (j + h - 1) % h);
-    SW = m_grid->getAt((i + w - 1) % w, (j + 1) % h);
-    SE = m_grid->getAt((i + 1) % w, (j + 1) % h);
+    int Ny = (j + h - 1) % h;
+    int Ey = j;
+    int Sy = (j + 1) % h;
+    int Wy = j;
+
+    // Cell values
+    int N = m_grid->getAt(Nx, Ny);
+    int E = m_grid->getAt(Ex, Ey);
+    int S = m_grid->getAt(Sx, Sy);
+    int W = m_grid->getAt(Wx, Wy);
+
+    int NW = m_grid->getAt(Wx, Ny);
+    int NE = m_grid->getAt(Ex, Ny);
+    int SW = m_grid->getAt(Wx, Sy);
+    int SE = m_grid->getAt(Ex, Sy);
 
     return NW + N + NE + W + E + SW + S + SE;
 }
