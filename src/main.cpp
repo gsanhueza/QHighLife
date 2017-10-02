7ec14e05137141891cb94c71670eb9380d6bd672
gsanhueza <gabriel_8032@hotmail.com>
2017-10-01 21:18:21 -0300

    Dibujando como triángulos 3D, con z=0

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <QApplication>
+#include <QSurfaceFormat>
 #include "qhighlife.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QHighLife viewer;
+
+    QSurfaceFormat glFormat;
+    glFormat.setVersion(3, 3);
+    glFormat.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(glFormat);
 
     viewer.show();
 
