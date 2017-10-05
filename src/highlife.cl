29f4779a9f5e78e452b0e246894f48e2c659c8a3
gsanhueza <gabriel_8032@hotmail.com>
2017-10-05 19:15:31 -0300

    WIP: Bases para variantes hechas en todas las implementaciones paralelas, falta hacerlas

--- src/highlife.cl
+++ src/highlife.cl
@@ -61,3 +61,34 @@ kernel void computeHighLife(global bool *grid, global bool *result, int width, i
         }
     }
 }
+
+// Kernel
+// FIXME Arreglar para que use el if (Hint: fixear surroundingAliveCells)
+kernel void computeHighLifeIf(global bool *grid, global bool *result, int width, int height)
+{
+    //     int i = (blockDim.x * blockIdx.x) + threadIdx.x; // CUDA Style
+    //     int j = (blockDim.y * blockIdx.y) + threadIdx.y;
+
+    //     int i = (get_local_size(0) * get_group_id(0)) + get_local_id(0); // OpenCL, CUDA Style
+    //     int j = (get_local_size(1) * get_group_id(1)) + get_local_id(1);
+
+    int i = get_global_id(0);                               // Get x=0 dimension
+    int j = get_global_id(1);                               // Get y=1 dimension
+
+    if (i < width && j < height)                            // Iterate only over our data
+    {
+        // Not 2 or 3 cells surrounding this alive cell = Cell dies
+        if (grid[getPos(i, j, width)] && !(surroundingAliveCells(grid, i, j, width, height) == 2 || surroundingAliveCells(grid, i, j, width, height) == 3))
+        {
+            result[getPos(i, j, width)] = 0;
+        }
+        // Dead cell surrounded by 3 or 6 cells = Cell revives
+        else if (!grid[getPos(i, j, width)] && (surroundingAliveCells(grid, i, j, width, height) == 3 || surroundingAliveCells(grid, i, j, width, height) == 6))
+        {
+            result[getPos(i, j, width)] = 1;
+        }
+        else{
+            result[getPos(i, j, width)] = grid[getPos(i, j, width)];
+        }
+    }
+}
--- src/openclmodel.cpp
+++ src/openclmodel.cpp
@@ -181,8 +181,72 @@ int OpenCLModel::runStressTest(int timeInSeconds)
 
 int OpenCLModel::runStressTestVariantIf(int timeInSeconds)
 {
-    // TODO Variante If
-    return -15;
+    // Filling data
+    for (int j = 0; j < m_grid->getHeight(); j++)
+    {
+        for (int i = 0; i < m_grid->getWidth(); i++)
+        {
+            host_grid[getPosCL(i, j, m_grid->getWidth())] = m_grid->getAt(i, j);
+        }
+    }
+
+    // Copy the input data to the input buffers using the command queue.
+    queue.enqueueWriteBuffer( buffer_grid, CL_FALSE, 0, m_grid->getWidth() * m_grid->getHeight() * sizeof(bool), host_grid );
+
+    // Make kernel
+    cl::Kernel highlife_kernel(program, "computeHighLifeIf");
+
+    // Set kernel dimensions
+    cl::NDRange global( m_grid->getWidth(), m_grid->getHeight() );
+    cl::NDRange local( 8, 8 );                              // 64 workitems per workgroup
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_start = std::chrono::high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_end = m_start + std::chrono::seconds(timeInSeconds);
+    int iterations = 0;
+
+    while (std::chrono::high_resolution_clock::now() < m_end)
+    {
+        // Optimization: We expect buffer_grid to be READONLY, and buffer_result to be WRITEONLY.
+        // We start with buffer_grid == buffer_result.
+        // When we finish the computation once, we (theoretically) want to update buffer_grid. => buffer_grid will be the same as buffer_result.
+        // If we (temporarily) use buffer_result as buffer_grid in each second computation, we'll get the same "start".
+        // Thus, our final results will be in buffer_grid. => We have to copy back buffer_grid to h_result to get the real result.
+        // With this, we can avoid calling enqueueReadBuffer every iteration.
+
+        // Set the kernel arguments, part 1
+        highlife_kernel.setArg(0, buffer_grid);
+        highlife_kernel.setArg(1, buffer_result);
+        highlife_kernel.setArg(2, m_grid->getWidth());
+        highlife_kernel.setArg(3, m_grid->getHeight());
+
+        // Execute the kernel, part 1
+        queue.enqueueNDRangeKernel( highlife_kernel, cl::NullRange, global, local );
+
+        // Set the kernel arguments, part 2
+        highlife_kernel.setArg(0, buffer_result);
+        highlife_kernel.setArg(1, buffer_grid);
+        highlife_kernel.setArg(2, m_grid->getWidth());
+        highlife_kernel.setArg(3, m_grid->getHeight());
+
+        // Execute the kernel, part 2
+        queue.enqueueNDRangeKernel( highlife_kernel, cl::NullRange, global, local );
+
+        iterations += 2;
+    }
+
+    // Copy the output data back to the host (Check note above to see why our results are in buffer_grid instead of buffer_result.)
+    queue.enqueueReadBuffer( buffer_grid, CL_TRUE, 0, m_grid->getWidth() * m_grid->getHeight() * sizeof(bool), host_result );
+
+    // Set the result
+    for (int j = 0; j < m_grid->getHeight(); j++)
+    {
+        for (int i = 0; i < m_grid->getWidth(); i++)
+        {
+            m_grid->setAt(i, j, host_result[getPosCL(i, j, m_grid->getWidth())]);
+        }
+    }
+
+    return iterations;
 }
 
 int OpenCLModel::runStressTestVariantNonIf(int timeInSeconds)
@@ -197,6 +261,70 @@ int OpenCLModel::runStressTestVariant32(int timeInSeconds)
 
 int OpenCLModel::runStressTestVariantNon32(int timeInSeconds)
 {
-    // TODO Variante No 32
-    return -45;
+    // Filling data
+    for (int j = 0; j < m_grid->getHeight(); j++)
+    {
+        for (int i = 0; i < m_grid->getWidth(); i++)
+        {
+            host_grid[getPosCL(i, j, m_grid->getWidth())] = m_grid->getAt(i, j);
+        }
+    }
+
+    // Copy the input data to the input buffers using the command queue.
+    queue.enqueueWriteBuffer( buffer_grid, CL_FALSE, 0, m_grid->getWidth() * m_grid->getHeight() * sizeof(bool), host_grid );
+
+    // Make kernel
+    cl::Kernel highlife_kernel(program, "computeHighLife");
+
+    // Set kernel dimensions
+    cl::NDRange global( m_grid->getWidth(), m_grid->getHeight() );
+    cl::NDRange local( 8, 8 );                              // FIXME Cambiar aqui para el no-32: 64 workitems per workgroup
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_start = std::chrono::high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_end = m_start + std::chrono::seconds(timeInSeconds);
+    int iterations = 0;
+
+    while (std::chrono::high_resolution_clock::now() < m_end)
+    {
+        // Optimization: We expect buffer_grid to be READONLY, and buffer_result to be WRITEONLY.
+        // We start with buffer_grid == buffer_result.
+        // When we finish the computation once, we (theoretically) want to update buffer_grid. => buffer_grid will be the same as buffer_result.
+        // If we (temporarily) use buffer_result as buffer_grid in each second computation, we'll get the same "start".
+        // Thus, our final results will be in buffer_grid. => We have to copy back buffer_grid to h_result to get the real result.
+        // With this, we can avoid calling enqueueReadBuffer every iteration.
+
+        // Set the kernel arguments, part 1
+        highlife_kernel.setArg(0, buffer_grid);
+        highlife_kernel.setArg(1, buffer_result);
+        highlife_kernel.setArg(2, m_grid->getWidth());
+        highlife_kernel.setArg(3, m_grid->getHeight());
+
+        // Execute the kernel, part 1
+        queue.enqueueNDRangeKernel( highlife_kernel, cl::NullRange, global, local );
+
+        // Set the kernel arguments, part 2
+        highlife_kernel.setArg(0, buffer_result);
+        highlife_kernel.setArg(1, buffer_grid);
+        highlife_kernel.setArg(2, m_grid->getWidth());
+        highlife_kernel.setArg(3, m_grid->getHeight());
+
+        // Execute the kernel, part 2
+        queue.enqueueNDRangeKernel( highlife_kernel, cl::NullRange, global, local );
+
+        iterations += 2;
+    }
+
+    // Copy the output data back to the host (Check note above to see why our results are in buffer_grid instead of buffer_result.)
+    queue.enqueueReadBuffer( buffer_grid, CL_TRUE, 0, m_grid->getWidth() * m_grid->getHeight() * sizeof(bool), host_result );
+
+    // Set the result
+    for (int j = 0; j < m_grid->getHeight(); j++)
+    {
+        for (int i = 0; i < m_grid->getWidth(); i++)
+        {
+            m_grid->setAt(i, j, host_result[getPosCL(i, j, m_grid->getWidth())]);
+        }
+    }
+
+    return iterations;
 }
